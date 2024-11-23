4440f8c39ae6588e4e46b03b63dfbcb6a9ccfe24
Alisa Perepelova <alice-lilo@mail.ru>
2024-11-23 22:10:15 +0300

    Initial commit

---
+++ src/test/java/PracticeFormTest.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+//import com.codeborne.selenide.Configuration;
+
+public class PracticeFormTest {
+
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/text-box");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#currentAddress").setValue("Some street 1");
+        $("#permanentAddress").setValue("Another street 1");
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("alex@egorov.com"));
+        $("#output #currentAddress").shouldHave(text("Some street 1"));
+        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+    }
+}
